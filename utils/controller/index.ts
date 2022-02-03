33de9b3360faf15b2666e776f5d5c76831440e70
Aleksander Dyatlov <aleksander.dyaloff@gmail.com>
2022-02-03 12:16:28 +0200

    Fixed providing node_modules types

--- utils/controller/index.ts
+++ utils/controller/index.ts
@@ -1,13 +1,11 @@
-const controller = (callback) => {
-  return async (req, res, next) => {
-    try {
-      await callback(req, res, next);
-    } catch (error) {
-      console.log(error);
+const controller = (callback) => async (req, res, next) => {
+  try {
+    await callback(req, res, next);
+  } catch (error) {
+    console.log(error);
 
-      res.json(error);
-    }
+    res.json(error);
   }
-}
+};
 
-export default controller
+export default controller;
